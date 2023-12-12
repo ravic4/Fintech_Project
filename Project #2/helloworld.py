df5ccf8833c3c4c03f89e1319bfaee47166867b9
ravic4 <ravi220299@gmail.com>
2023-12-11 19:55:05 -0800

    comment

---
+++ Project #2/helloworld.py
@@ -0,0 +1,6 @@
+#Writing hello world 
+print("hello world") 
+
+#Print a statement such as: "Hello there this is my first python line" but add a break line before 
+print("--------------")
+print("hello tehre this is imy first python line") 
