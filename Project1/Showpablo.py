6593ae6d0501d4130489e1e1bbe3f96ca877a4ac
ravic4 <ravi220299@gmail.com>
2023-12-12 21:15:34 -0800

    Added information december 12 Mercado Libre

---
+++ Project1/Showpablo.py
@@ -0,0 +1,32 @@
+# Percent Increase Bonus Activity
+
+# Formulas
+Apple_current_price = 254.32
+Apple_prior_price = 198.87
+
+# Increase = Current Price - Original Price
+Increase = Apple_current_price - Apple_prior_price
+print(Increase)
+# Percent Increase = Increase / Original x 100
+Percent_increase = ((Apple_current_price)/(Apple_prior_price))*100
+
+# Create float variable for original_price
+float(Apple_prior_price)
+
+# Create float variable for current_price
+float(Apple_prior_price)
+
+# Calculate difference between current_price and original_price
+Price_difference = Apple_current_price-Apple_prior_price
+print(Price_difference) ### Watch out for capitalized letters 
+# Calculate percent_increase
+#Print(Percent_increase)
+
+# Print original_price
+print(f"Apple's original stock price was ${Apple_prior_price}")
+
+# Print current_price
+print(f"Apples current stock price is ${Apple_current_price}")
+
+# Print percent_increase
+print(f"Apple's percent increase is: ${Percent_increase}")
